c27580acee1931ceaca79ef2bd8aab87d11e9003
Javier Montesinos <fj.montesinos@gmail.com>
2017-03-04 00:31:07 +0100

    Step 2.5. Displaying heroes with a template

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -15,9 +15,22 @@ var Hero = (function () {
     return Hero;
 }());
 exports.Hero = Hero;
+var HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
+        this.heroes = HEROES;
         this.hero = {
             id: 2,
             name: 'Windstorm'
@@ -26,7 +39,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <h1>{{title}}</h1>\n    <h2>{{hero.name}} details!</h2>\n    <div><label>Id : </label> {{hero.id}}</div>\n    <div>\n      <label>Name : </label> \n      <input [(ngModel)]=\"hero.name\" placeholder=\"name\" />\n    </div>\n  ",
+            template: "\n    <h1>{{title}}</h1>\n    \n    <h2>My Heros</h2>\n    <ul class=\"heroes\">\n      <li *ngFor=\"let hero of heroes\">\n        <!-- cada heroe ir\u00E1 aqu\u00ED -->\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n    \n    <h2>{{hero.name}} details!</h2>\n    <div><label>Id : </label> {{hero.id}}</div>\n    <div>\n      <label>Name : </label> \n      <input [(ngModel)]=\"hero.name\" placeholder=\"name\" />\n    </div>\n  ",
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
