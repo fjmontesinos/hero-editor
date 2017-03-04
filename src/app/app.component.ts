78bbe8bacfe7a75bfc033e369a3db3e5ac410bf3
Javier Montesinos <fj.montesinos@gmail.com>
2017-03-04 01:11:29 +0100

    Step 3. Multiple Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id : number;
-  name : string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -32,15 +28,8 @@ const HEROES: Hero[] = [
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>Id : </label> {{selectedHero.id}}</div>
-      <div>
-        <label>Name : </label> 
-        <input [(ngModel)]="selectedHero.name" placeholder="name" />
-      </div>
-    </div>
+
+    <hero-detail [hero]="selectedHero"></hero-detail>
   `,
 
   // MUY IMPORTANTE
