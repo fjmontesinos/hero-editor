ea39d7f64c99b67d1df0bd318e98773dfe00a168
Javier Montesinos <fj.montesinos@gmail.com>
2017-03-07 06:35:16 +0100

    Step 6.1. Replace mock with Http Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './services/hero.in-memory.service';
 
 import { AppComponent }  from './app.component';
 import { MyHeroesComponent }  from './my-heroes.component';
@@ -17,6 +22,8 @@ import { HeroService } from './hero.service';
   imports:      [ 
       BrowserModule, 
       FormsModule ,
+      HttpModule,
+      InMemoryWebApiModule.forRoot(InMemoryDataService),
       AppRoutingModule
     ],
 
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
 import { Hero } from './hero';
-import { HEROES } from './hero.mock';
+
 /**
  * The consumer of our service doesn't know how the service gets the data. 
  * Our HeroService could get Hero data from anywhere. 
@@ -9,18 +13,37 @@ import { HEROES } from './hero.mock';
  */
 @Injectable()
 export class HeroService {
+
     // Milisegundos para la llamada a la carga de los héroes
-    ms : number;
+    private ms : number;
+    private heroesUrl = 'api/heroes';  // URL to web api
+
+    constructor(private http: Http) { }
 
     getHeroes () : Promise<Hero[]> {
         // Milisegundos entre 0 y 3
-        this.ms = (Math.floor(Math.random() * 3) + 0) * 1000;
+        //this.ms = (Math.floor(Math.random() * 3) + 0) * 1000;
 
         // Llamada a promise para simular un delay
-        return new Promise((resolve) => setTimeout(() => resolve(HEROES), this.ms));
+        // return new Promise((resolve) => setTimeout(() => resolve(HEROES), this.ms));
+        return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => response.json().data as Hero[])
+               .catch(this.handleError);
+    }
+
+    handleError(error : any){
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
 
     getHero(id: number): Promise<Hero> {
-        return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+        const url = `${this.heroesUrl}/${id}`;
+
+        //return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+        return this.http.get(url)
+            .toPromise()
+            .then(response => response.json().data as Hero)
+            .catch(this.handleError);
     }
 }
